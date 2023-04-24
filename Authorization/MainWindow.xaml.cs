33b3f3455b0ddad55c506b49122a5b569db089fb
KarinaShcherbakova <karina.shcherbakova.96@mail.ru>
2023-04-24 09:51:08 +0700

    Добавьте файлы проекта.

---
+++ Authorization/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model1Container db;
+        public MainWindow()
+        { 
+            InitializeComponent();
+            db = new Model1Container();
+        }
+
+        private void Authorization_Click(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == "" || password.Password == "")
+            {
+                MessageBox.Show("Ошибка пустые поля");
+            }
+            if (db.Users.Select(item => item.Login + " " + item.Password).Contains(login.Text + " " + password.Password))
+            {
+                MessageBox.Show("Вы авторизированы");
+            }
+            else
+            {
+                MessageBox.Show("Ошибка логина/пароля");
+            }
+
+        }
+
+        private void Regist_Click(object sender, RoutedEventArgs e)
+        {
+            RegWindow rw = new RegWindow();
+            rw.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Authorization/RegWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для RegWindow.xaml
+    /// </summary>
+    public partial class RegWindow : Window
+    {
+        Model1Container db = new Model1Container();
+        public RegWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationClick(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == "" || password.Password == "" || lastName.Text == "" || middleName.Text == "")
+            {
+                MessageBox.Show("Ошибка пустые поля");
+                return;
+            }
+            if (db.Users.Select(item => item.Login).Contains(login.Text))
+            {
+                MessageBox.Show("Такой логин существует в системе");
+                return;
+            }
+            User newUser = new User()
+            {
+                Login = login.Text,
+                Password = password.Password,
+                LastName = lastName.Text,// добавить проверку на имя
+                MiddleName = middleName.Text
+            };
+            db.Users.Add(newUser);
+            db.SaveChanges();
+            MessageBox.Show("Вы успешно зарегистрировались");
+            MainWindow aw = new MainWindow();
+            aw.Show();
+            this.Close();
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            RegWindow rw = new RegWindow();
+            rw.Show();
+            this.Close();
+        }
+    }
+}
